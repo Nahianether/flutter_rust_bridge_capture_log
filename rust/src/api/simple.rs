c3ef43bf68ee3db87aa30bc98ca837620be3e619
Nahianether <nahianether3@gmail.com>
2024-09-19 15:10:04 +0600

    log wip (code commented)

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,12 +1,12 @@
 use screenshots::Screen;
-use std::ffi::OsString;
-use windows::core::Result;
-use windows::Win32::System::Diagnostics::Etw::{
-    EvtQuery, EvtQueryReverseDirection, EVT_QUERY_FLAGS,
-};
-use windows::Win32::System::EventLog::{
-    EvtNext, EvtRender, EvtRenderEventXml, EVT_HANDLE,
-};
+// use std::ffi::OsString;
+// use windows::core::Result;
+// use windows::Win32::System::Diagnostics::Etw::{
+//     EvtQuery, EvtQueryReverseDirection, EVT_QUERY_FLAGS,
+// };
+// use windows::Win32::System::EventLog::{
+//     EvtNext, EvtRender, EvtRenderEventXml, EVT_HANDLE,
+// };
 
 #[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
 pub fn greet(name: String) -> String {
@@ -33,52 +33,52 @@ pub fn take_s(path: String) {
     // "done".to_string()
 }
 
-#[flutter_rust_bridge::frb(sync)]
-pub fn get_windows_logs() -> String {
-    let query = OsString::from("Application");
-    let flags = EVT_QUERY_FLAGS::EvtQueryReverseDirection;
-    let h_results = unsafe { EvtQuery(None, &query, None, flags) };
+// #[flutter_rust_bridge::frb(sync)]
+// pub fn get_windows_logs() -> String {
+//     let query = OsString::from("Application");
+//     let flags = EVT_QUERY_FLAGS::EvtQueryReverseDirection;
+//     let h_results = unsafe { EvtQuery(None, &query, None, flags) };
 
-    if h_results.is_err() {
-        return "Failed to query event logs.".to_string();
-    }
+//     if h_results.is_err() {
+//         return "Failed to query event logs.".to_string();
+//     }
 
-    let h_results = h_results.unwrap();
-    let mut buffer = vec![0u8; 1024];
-    let mut buffer_used = 0;
-    let mut property_count = 0;
+//     let h_results = h_results.unwrap();
+//     let mut buffer = vec![0u8; 1024];
+//     let mut buffer_used = 0;
+//     let mut property_count = 0;
 
-    let mut logs = String::new();
-    while unsafe {
-        EvtNext(
-            h_results,
-            1,
-            &mut buffer as *mut _ as *mut EVT_HANDLE,
-            0,
-            0,
-            &mut property_count,
-        )
-    }
-    .is_ok()
-    {
-        let status = unsafe {
-            EvtRender(
-                None,
-                buffer[0] as EVT_HANDLE,
-                EvtRenderEventXml,
-                buffer.len() as u32,
-                buffer.as_mut_ptr() as *mut _,
-                &mut buffer_used,
-                &mut property_count,
-            )
-        };
-        if status.is_err() {
-            break;
-        }
+//     let mut logs = String::new();
+//     while unsafe {
+//         EvtNext(
+//             h_results,
+//             1,
+//             &mut buffer as *mut _ as *mut EVT_HANDLE,
+//             0,
+//             0,
+//             &mut property_count,
+//         )
+//     }
+//     .is_ok()
+//     {
+//         let status = unsafe {
+//             EvtRender(
+//                 None,
+//                 buffer[0] as EVT_HANDLE,
+//                 EvtRenderEventXml,
+//                 buffer.len() as u32,
+//                 buffer.as_mut_ptr() as *mut _,
+//                 &mut buffer_used,
+//                 &mut property_count,
+//             )
+//         };
+//         if status.is_err() {
+//             break;
+//         }
 
-        logs.push_str(&String::from_utf8_lossy(&buffer[..buffer_used as usize]));
-        logs.push('\n');
-    }
+//         logs.push_str(&String::from_utf8_lossy(&buffer[..buffer_used as usize]));
+//         logs.push('\n');
+//     }
 
-    logs
-}
+//     logs
+// }
