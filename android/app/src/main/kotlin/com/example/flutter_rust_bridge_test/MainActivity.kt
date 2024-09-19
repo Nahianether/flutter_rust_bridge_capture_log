cceaa9a254d959b26df1aa395d9e9f4ae9b29c25
Nahianether <nahianether3@gmail.com>
2024-09-19 14:56:04 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_rust_bridge_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_rust_bridge_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rust_bridge_test/src/rust/api/simple.dart';
+import 'package:flutter_rust_bridge_test/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+              ElevatedButton(
+                onPressed: () async {
+                  takeS(path: '/Users/intishar/Pictures/screenshot.png');
+                },
+                child: const Text('ScreenShot'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // String logs = await getWindowsLogs();
+                  // debugPrint('Logs: $logs');
+                },
+                child: const Text('Get Windows Logs'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
